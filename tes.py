d6525ade03c7ba4e3869252b4e0e0d0f40fbfc41
Antonio Setya <antonio_setya@hotmail.com>
2018-11-16 16:58:00 +0700

    Fixes to frontend not passing all data

--- tes.py
+++ tes.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request
+from sklearn.naive_bayes import GaussianNB
 import json
+from sklearn.externals import joblib
+from traceback import print_exc
 app = Flask(__name__)
 
 Flask.debug = True
+
+loaded_model = joblib.load("NB.dat")
 
 @app.route("/", methods=['GET'])
 def home():
@@ -22,19 +27,31 @@ def process():
         fasting_blood = int(request.form["fasting_blood_sugar"]);
         resting_ecg = int(request.form["resting_ecg"]);
         max_heart_rate = int(request.form["max_heart_rate"]);
+        exc_induced = int(request.form["exc_induced"]);
         st_depression = int(request.form["st_depression"]);
         peak_exercise = int(request.form["peak_exercise"]);
         major_fluorosopy = int(request.form["major_fluorosopy"]);
         thal = int(request.form["thal"]);
 
-        response_text = "Gender : " + str(gender) + " | Age : " + str(age) + " | Chest Pain : " + str(chest_pain);
+        new_instance = [gender,age,chest_pain,blood_pres,serum,fasting_blood,resting_ecg,max_heart_rate, exc_induced, st_depression, peak_exercise, major_fluorosopy, thal];
+
+        prediction = loaded_model.predict([new_instance]);
+        print(prediction)
+        if (prediction[0] == 0):
+            response_text = "You don't have heart disease!";
+        else:
+            response_text = "You have heart disease!";
+
     except(ValueError):
+        print_exc();
         response_text = "Parsing error!";
         status = -1;
     except(KeyError):
+        print_exc();
         response_text = "Please fill in all the fields!";
         status = -2;
     except:
+        print_exc();
         response_text = "Unhandled exception occured!";
         status = -999;
     
