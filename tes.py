f530a0ba113559ca7611fc7e72489d5cd86b617e
Antonio Setya <13516002@std.stei.itb.ac.id>
2018-11-25 11:33:05 +0700

    Change UI library & imperfect predictor

--- tes.py
+++ tes.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request
-from sklearn.tree import DecisionTreeClassifier
 import json
+import numpy as np
+from pandas import read_csv
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import RobustScaler
 from sklearn.externals import joblib
 from traceback import print_exc
 app = Flask(__name__)
 
 Flask.debug = True
 
-loaded_model = joblib.load("TREE.dat")
+loaded_model = joblib.load("KNN.dat")
+data_transformer = joblib.load("data_transformer.dat")
 
 @app.route("/", methods=['GET'])
 def home():
@@ -19,23 +23,30 @@ def process():
     response_text = ""
     try:
         # Retrieving request data
-        gender = int(request.form["gender"]);
-        age = int(request.form["age"]);
-        chest_pain = int(request.form["chest_pain"]);
-        blood_pres = int(request.form["blood_pres"]);
-        serum = int(request.form["serum"]);
-        fasting_blood = int(request.form["fasting_blood_sugar"]);
-        resting_ecg = int(request.form["resting_ecg"]);
-        max_heart_rate = int(request.form["max_heart_rate"]);
-        exc_induced = int(request.form["exc_induced"]);
-        st_depression = float(request.form["st_depression"]);
-        peak_exercise = int(request.form["peak_exercise"]);
-        major_fluorosopy = int(request.form["major_fluorosopy"]);
-        thal = int(request.form["thal"]);
+        gender = np.int64(request.form["gender"]);
+        age = np.int64(request.form["age"]);
+        chest_pain = np.int64(request.form["chest_pain"]);
+        blood_pres = np.int64(request.form["blood_pres"]);
+        serum = np.int64(request.form["serum"]);
+        fasting_blood = np.int64(request.form["fasting_blood_sugar"]);
+        resting_ecg = np.int64(request.form["resting_ecg"]);
+        max_heart_rate = np.int64(request.form["max_heart_rate"]);
+        exc_induced = np.int64(request.form["exc_induced"]);
+        st_depression = np.float64(request.form["st_depression"]);
+        peak_exercise = np.int64(request.form["peak_exercise"]);
+        major_fluorosopy = np.int64(request.form["major_fluorosopy"]);
+        thal = np.int64(request.form["thal"]);
 
-        new_instance = [age,gender,chest_pain,blood_pres,serum,fasting_blood,resting_ecg,max_heart_rate, exc_induced, st_depression, peak_exercise, major_fluorosopy, thal];
+        # Constructing into a new instance
+        new_instance = [[age,gender,chest_pain,blood_pres,serum,fasting_blood,resting_ecg,max_heart_rate, exc_induced, st_depression, peak_exercise, major_fluorosopy, thal]];
+        print(new_instance);
 
-        prediction = loaded_model.predict([new_instance]);
+        # Transform new instance so that it's compatible with the model
+        new_instance = data_transformer.transform(new_instance);
+        print(new_instance);
+
+        # Predict instance
+        prediction = loaded_model.predict(new_instance);
         print(prediction);
         response_text = str(prediction[0]);
         """
